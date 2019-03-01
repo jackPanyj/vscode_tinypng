1001195c22058e532e8fc9786ce78306e85e390f
jackpan <yanjie.pan@17zuoye.com>
2019-03-01 19:52:19 +0800

    save

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,24 @@
 
 import tinify from 'tinify';
 import * as vscode from 'vscode';
+
+const compressImage = (file: any) => {
+	const status = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+	status.text = '正在压缩图片' + file.path;
+	status.show();
+	const source = tinify.fromFile(file.path);
+	source.toFile(file.path, (err) => {
+		status.hide();
+		if (err) {
+			compressImage(file);
+		}
+	});
+};
+
+
 export function activate(context: vscode.ExtensionContext) {
 	tinify.key = 'H59Q1cgdhz7FFqQDhp2x3QNnmN56jzyT';
-	let disposable = vscode.commands.registerCommand('extension.compressFile', (file) => {
-		tinify.fromFile(file.path).toFile(file.path);
-	});
+	let disposable = vscode.commands.registerCommand('extension.compressFile', compressImage);
 
 	context.subscriptions.push(disposable);
 }
